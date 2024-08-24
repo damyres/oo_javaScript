3e9189d453b6f840c3ffee4478e66e1e4a2c06a4
Damyres <damyresmaciell@gmail.com>
2024-08-24 18:23:43 -0300

    Usar herança classes e composição

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Personagem } from "./modules/personagem.js"
 import { PerersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 
 /*const personagemPedrinho = {
     nome: 'Pedrinho',
@@ -69,7 +70,8 @@ personagemPedro.level = 2*/
 const magoAntonio = new Mago('Antonio', 2, 'fogo', 4, 3)
 const magaJulia = new Mago('Júlia', 8, 'gelo', 7, 10)
 const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroBruno]
+const personagens = [magoAntonio, magaJulia, arqueiroBruno,arqueiroMagoChico]
 
 new PerersonagemView(personagens).render()
